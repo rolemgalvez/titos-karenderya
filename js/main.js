fd24166122f690056376a88b425ea01e1a49b8cf
Mark Rolem Galvez <markrolem.galvez15@gmail.com>
2020-02-21 11:32:54 -0800

    Sticky Navbar

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ function initMap() {
   // The marker, positioned and location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Sticky Navbar Opacity
+window.addEventListener('scroll', function() {
+  if(window.scrollY > 150) {
+    this.document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    this.document.querySelector('#navbar').style.opacity = 1;
+  }
+});
